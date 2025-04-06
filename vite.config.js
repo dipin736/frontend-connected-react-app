916943251e40229a015f22a8da2e813992eb2739
Dipin Karunakaran <114206930+dipin736@users.noreply.github.com>
2025-04-06 22:31:25 +0530

    removed

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // This must match Render's "Publish Directory"
-  },
-  base: '/' // Ensure routing works properly on refresh
 })
