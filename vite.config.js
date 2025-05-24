8dfd92ba5cafa287293678621743abd5d7466c9d
Dipin Karunakaran <114206930+dipin736@users.noreply.github.com>
2025-05-24 18:51:07 +0530

    Fix Node.js crypto issue and update workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import NodePolyfills from 'vite-plugin-node-polyfills';
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), NodePolyfills()],
+  define: {
+    'process.env': {},
+  },
 });
